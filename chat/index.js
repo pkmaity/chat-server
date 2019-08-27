b569572cb41df3180e322e096204f04635a4658a
Bhaskar <bhaskar.mitra@techprostudio.com>
2019-08-27 20:19:27 +0530

    new event

--- chat/index.js
+++ chat/index.js
@@ -19,6 +19,8 @@ module.exports.socketServer = (http) => {
                 activeUsers.push( {rooms: data, sockets: socket.id} );
             }
 
+            socket.broadcast.emit('new active user', activeUsers.map(val => val.rooms));
+
         });
 
         socket.on('disconnect', function(){
@@ -35,14 +37,36 @@ module.exports.socketServer = (http) => {
             /**
              * Sending other users that one user is offline
              */
-            
             socket.broadcast.emit('offline user', users[ activeUsers.findIndex((val) => { return (val.sockets == socket.id) } ) ] );
-            
+
             /**
              * Deactivating online user from activated user list
              */
             activeUsers.splice(sockets.findIndex((val) => { return (val == socket.id) } ), 1 );
             
+        });
+
+        socket.on('disconnect user', (data) => {
+
+            /**
+             * List of Users
+             */
+            let users = activeUsers.map(val => val.rooms);
+
+            /**
+             * Sending other users that one user is offline
+             */
+            socket.broadcast.emit('offline user', users[ activeUsers.findIndex((val) => { return (val.rooms === data) } ) ] );
+
+            /**
+             * Deactivating online user from activated user list
+             */
+            activeUsers.splice(users.findIndex((val) => { return (val === data) } ), 1 );
+
+        });
+
+        socket.on('online users', () => {
+            socket.broadcast.emit('new active user', activeUsers.map(val => val.rooms));
         });
 
         socket.on('send message', (data) => {
